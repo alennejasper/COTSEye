840145cc4f465f416aed8ed0e1d3de7e5e4f7d6e
alennejasper <alennejasper@gmail.com>
2024-06-17 15:35:34 +0800

    update

--- statics/templates/webwares/application.js
+++ statics/templates/webwares/application.js
@@ -1,6 +1,11 @@
 if ("serviceWorker" in navigator){
-    navigator.serviceWorker.register("/cache.js", {scope: "."})
+    navigator.serviceWorker.register("/cache.js", {scope: "/contributor"})
         .then((register) => console.log("The service worker or cache for COTSEye is now registered.", register))
 
-        .catch((error) => console.log("The service worker or cache for COTSEye cannot be registered.", error))
-}
+        .catch((error) => console.log("The service worker or cache for COTSEye cannot be registered.", error));
+    
+    navigator.serviceWorker.register("/cache.js", {scope: "/public"})
+        .then((register) => console.log("The service worker or cache for COTSEye is now registered.", register))
+
+        .catch((error) => console.log("The service worker or cache for COTSEye cannot be registered.", error));
+};
--- statics/templates/webwares/cache.js
+++ statics/templates/webwares/cache.js
@@ -1,6 +1,8 @@
 self.addEventListener("install", event => {
     console.log("The service worker or cache for COTSEye has been installed.");
 
+    self.skipWaiting();
+
     event.waitUntil(
         caches.open("service/index").then(cache => {
             cache.addAll([                    
@@ -52,10 +54,8 @@ self.addEventListener("install", event => {
                 
                 "/statics/assets/icons/logo (512 x 512).png",
 
-
-                "https://fonts.googleapis.com/css2?family=Philosopher:wght@400;700&display=swap",
                 
-                "https://fonts.googleapis.com/css2?family=Overlock:wght@400;700;900&display=swap",
+                "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;900&display=swap",
                 
                 "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css",
 
@@ -79,11 +79,13 @@ self.addEventListener("activate", event => {
 
     event.waitUntil(
         caches.keys().then(keys => {
-            return Promise.all(
-                keys.filter( key => key !== "service/index" && key !== "service/whole").map(key => caches.delete(key))
+            return Promise.all(                
+                keys.filter( key => key === "service/whole").map(key => caches.delete(key))
             )
         })
-    );    
+    );      
+    
+    self.clients.claim()
 });
 
   
@@ -93,60 +95,38 @@ self.addEventListener("fetch", event => {
     const limit = (name, size) => {
         caches.open(name).then(cache => {
             cache.keys().then(keys => {
-                if(keys.length > size){
+                if (keys.length > size) {
                     cache.delete(keys[0]).then(() => limit(name, size));
-                };
-            });
+                }
+            })
         })
     };
 
     event.respondWith(
-        caches.match(event.request).then(response => {
-            if(response){
-                const expiration = 1 * 60 * 1000;
+        fetch(event.request).then(response => {
+            const clone = response.clone();
 
-                const today = new Date().getTime();
-                
-                const age = new Date(response.headers.get("date")).getTime();
-
-                if(today - age > expiration){
-                    return fetch(event.request).then(fetch => {
-                        const clone = fetch.clone();
-
-                        caches.open("service/whole").then(cache => {
-                            cache.put(event.request, clone);
-
-                            limit("service/whole", 50);
-                        });
-
-                        return fetch;
-                    });
-
-                } else{
+            caches.open("service/whole").then(cache => {
+                cache.put(event.request, clone);
+            
+                limit("service/whole", 500);
+            });
+            
+            return response;
+            
+        }).catch(() => {
+            return caches.match(event.request).then(response => {
+                if (response){
                     return response;
                 };
-                
-            } else{
-                return fetch(event.request).then(response => {
-                    const clone = response.clone();
 
-                    caches.open("service/whole").then(cache => {
-                        cache.put(event.request, clone);
+                if (event.request.url.includes("contributor/")){
+                    return caches.match("contributor/service/fallback/");
 
-                        limit("service/whole", 50);
-                    });
-
-                    return response;
-
-                }).catch(() => {
-                    if (event.request.url.includes("contributor/")) {
-                        return caches.match("contributor/service/fallback/");
-
-                    } else {
-                        return caches.match("public/service/fallback/");
-                    };
-                });
-            };
+                } else{
+                    return caches.match("public/service/fallback/");
+                };
+            });
         })
     );
 });
