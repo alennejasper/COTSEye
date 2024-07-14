16246f430fc0790ea2fa0c2dfba427beea88baff
alennejasper <alennejasper@gmail.com>
2024-07-14 21:59:02 +0800

    The updated version of COTSEye (July 2024).

--- statics/templates/webwares/application.js
+++ statics/templates/webwares/application.js
@@ -1,10 +1,5 @@
 if ("serviceWorker" in navigator){
-    navigator.serviceWorker.register("/cache.js", {scope: "/contributor"})
-        .then((register) => console.log("The service worker or cache for COTSEye is now registered.", register))
-
-        .catch((error) => console.log("The service worker or cache for COTSEye cannot be registered.", error));
-    
-    navigator.serviceWorker.register("/cache.js", {scope: "/public"})
+    navigator.serviceWorker.register("/cache.js", {scope: "/"})
         .then((register) => console.log("The service worker or cache for COTSEye is now registered.", register))
 
         .catch((error) => console.log("The service worker or cache for COTSEye cannot be registered.", error));
