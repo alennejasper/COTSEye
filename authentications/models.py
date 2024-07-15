6ed19ca6d660184bc09d3d2129eacf3d129d59bf
alennejasper <alennejasper@gmail.com>
2024-07-15 11:30:37 +0800

    The updated version of COTSEye (July 2024).

--- authentications/models.py
+++ authentications/models.py
@@ -132,10 +132,10 @@ class Notification(models.Model):
     contenttype = models.ForeignKey(ContentType, on_delete = models.CASCADE)
     key = models.PositiveIntegerField()
 
-    # class Meta:
-    #     db_table = "auth_notification"
-    #     verbose_name = "Notification"
-    #     verbose_name_plural = "Notifications"
+    class Meta:
+        db_table = "auth_notification"
+        verbose_name = "Notification"
+        verbose_name_plural = "Notifications"
 
     @staticmethod
     def get_user_notifications(user):
